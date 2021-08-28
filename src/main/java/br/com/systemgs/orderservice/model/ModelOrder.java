6c78a9a2f31fe637dfd8227bd6f01db464a9aa9e
GuilhermeJWT <guiromanno@gmail.com>
2021-08-28 12:56:49 -0300

    Mudando a conexão para Postgres

--- src/main/java/br/com/systemgs/orderservice/model/ModelOrder.java
+++ src/main/java/br/com/systemgs/orderservice/model/ModelOrder.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "order")
+@Table(name = "order_tb")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,7 +16,7 @@ public class ModelOrder implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String name;
     private int qty;
     private double price;
