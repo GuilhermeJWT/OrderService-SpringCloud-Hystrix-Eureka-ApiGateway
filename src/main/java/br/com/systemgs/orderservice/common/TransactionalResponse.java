46dc310a719d5ea7dce974ecd5ae985bd1fb187d
GuilhermeJWT <guiromanno@gmail.com>
2021-08-28 14:52:54 -0300

    Primeira Integração para o PaymentService - RestTemplate Salvar

--- src/main/java/br/com/systemgs/orderservice/common/TransactionalResponse.java
+++ src/main/java/br/com/systemgs/orderservice/common/TransactionalResponse.java
@@ -1,11 +1,18 @@
 package br.com.systemgs.orderservice.common;
 
 import br.com.systemgs.orderservice.model.ModelOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionalResponse {
 
     private ModelOrder modelOrder;
-    private int amount;
+    private double amount;
     private String transactionalId;
+    private String message;
 
 }
--- src/main/java/br/com/systemgs/orderservice/service/OrderService.java
+++ src/main/java/br/com/systemgs/orderservice/service/OrderService.java
@@ -1,9 +1,13 @@
 package br.com.systemgs.orderservice.service;
 
+import br.com.systemgs.orderservice.common.Payment;
+import br.com.systemgs.orderservice.common.TransactionalRequest;
+import br.com.systemgs.orderservice.common.TransactionalResponse;
 import br.com.systemgs.orderservice.model.ModelOrder;
 import br.com.systemgs.orderservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -11,8 +15,22 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public ModelOrder salvarOrder(ModelOrder modelOrder){
-        return orderRepository.save(modelOrder);
+    @Autowired
+    private RestTemplate template;
+
+    public TransactionalResponse salvarOrder(TransactionalRequest transactionalRequest){
+        String response = "";
+        ModelOrder modelOrder = transactionalRequest.getModelOrder();
+        Payment payment = transactionalRequest.getPayment();
+        payment.setOrderId(modelOrder.getId());
+        payment.setAmount(modelOrder.getPrice());
+
+        Payment paymentResponse =  template.postForObject("http://localhost:9191/api/payment/salvar", payment, Payment.class);
+        orderRepository.save(modelOrder);
+
+        response = paymentResponse.getPaymentStatus().equals("success")? "payment processing succesful and order placed" : "there is a failure in payment api, order added to cart";
+
+        return new TransactionalResponse(modelOrder, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
     }
 
 }
